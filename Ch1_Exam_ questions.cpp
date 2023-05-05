1fff6a53cd34384fa46fe11f913f8c3b175b6088
Attikash23 <123202790+Attikash23@users.noreply.github.com>
2023-05-05 13:37:11 -0400

    Add files via upload

---
+++ Ch1_Exam_ questions.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+// Problem 27.
+ /*  Int speed = 20
+Int time = 10
+Int distance = time * speed
+Cout << distance << endl; */
+  
+// Problem 28.
+/*  Double force = 172.5
+Double area = 27.5
+Double pressure = 27.5 / 172.5
+Cout << pressure << endl; */
+
+  // Problem 5.
+/*  Int a = 12 * x
+Int z = (5 * x) + (14*y) + (6*k)
+Int y = pow(x,4)
+Int g = (b +12) / (4*k)
+Int c = pow(a,3) / (pow(b,2) * pow(k,4)) */
+
+  // problem 12.
+/*  cout << fixed<< showpoint<< setprecision(2);
+cout<< setw(8)<< divsales<<endl; */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+}
